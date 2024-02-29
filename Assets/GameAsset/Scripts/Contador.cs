2d860e1639d83c6577dbf0e7486fc97e22632c82
fenixblanco18 <montero.helena9@gmail.com>
2024-02-29 20:51:54 +0100

    contando columnas

--- Assets/GameAsset/Scripts/Contador.cs
+++ Assets/GameAsset/Scripts/Contador.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Contador : MonoBehaviour
 {
+    public Text textObject;
     public int contador = 0;
     public int puntosPorColumna = 1; 
     
     void OnTriggerExit(){
         contador=contador + puntosPorColumna;
+        textObject.GetComponentInChildren<TextMeshProUGUI>().SetText(contador.ToString());
     }
 }
