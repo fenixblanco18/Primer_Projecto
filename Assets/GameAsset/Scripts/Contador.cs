714f6edc9b23550bf466b57adb1dc8feea4c0d9a
fenixblanco18 <montero.helena9@gmail.com>
2024-03-01 16:14:20 +0100

    ajustando contador

--- Assets/GameAsset/Scripts/Contador.cs
+++ Assets/GameAsset/Scripts/Contador.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class Contador : MonoBehaviour
 {
-    public Text textObject;
+    public TMP_Text textObject;
     public int contador = 0;
     public int puntosPorColumna = 1; 
     
