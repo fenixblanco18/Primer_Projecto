8b796c0d7bb437126be288178681e11c41a56a8b
fenixblanco18 <montero.helena9@gmail.com>
2024-03-01 19:54:54 +0100

    añadiendo sonido salto

---
+++ Assets/GameAsset/Scripts/UIScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public void Reset(){
+        SceneManager.LoadScene(0);
+    }
+    // Start is called before the first frame update
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+}
